f9ee77a58894f7f4283e2bb54012b7d8b3875e0b
GideonAssafuah <gideon@walulel.com>
2021-06-23 21:27:01 +0000

    dawn changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="pb-10">
     <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+    <!-- main section -->
       <div class=" grid grid-cols-7 h-screen overflow-x-hidden ">
 
         <div class="flex justify-center items-center col-span-3">
@@ -16,6 +17,12 @@
         <div class="col-span-4">
           <img class="image-banner" src="~static/img/home-one/main-banner.png" alt="image">
         </div>
+      </div>
+      <!-- end main section -->
+
+      <div class=" mt-15 text-center">
+        <h2>New <span class="text-primary">opportunities</span></h2>
+
       </div>
     </div>
   </div>
